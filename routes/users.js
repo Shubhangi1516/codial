d2181d08bfe4f9ade7b6cb6607cad2cfa5264318
Shubhangi Shree <shubhangishree25@gmail.com>
2023-07-26 18:23:46 +0530

    create sign up

--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,14 @@ const router=express.Router();
 
 const usersController=require('../controllers/user_controller');
 router.get('/profile',usersController.profile);
+
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
+
+
+
+
+
+
 module.exports=router;
