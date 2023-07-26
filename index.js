4d663d6a2485405051821e4e4faee48ae7369d73
Shubhangi Shree <shubhangishree25@gmail.com>
2023-07-26 15:46:44 +0530

    setup static files access for scripts and styles

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(express.static('./assests'));
+
+app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 //use express router
 app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err){
